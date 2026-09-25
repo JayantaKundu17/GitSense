3650722f2651f5bc1770760b67b0e9112ad5d00c
Jayanta Kundu <jkundu918@gmail.com>
2026-09-25 10:56:47 +0530

    Allow Cloudflare frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -46,6 +46,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://gitsense.pages.dev",
     ],
     allow_credentials=True,
     allow_methods=["*"],
